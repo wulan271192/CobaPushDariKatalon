87d694a9ad17118e04451f50cd24a93308cb810b
userd <userd@DESKTOP-9E7T8R8>
2025-06-09 18:31:53 +0700

    commit wulan

---
+++ Object Repository/button_make_Appointment.rs
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>button_make_Appointment</name>
+   <tag></tag>
+   <elementGuidId>b86e2ac5-88fe-4d52-a524-46ed3a55c6c5</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@id = 'btn-make-appointment']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>btn-make-appointment</value>
+      <webElementGuid>2cf6418e-ee55-4ea3-b356-fd882def9b69</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
